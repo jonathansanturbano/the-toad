c518720330036eb2461c8a008f043c8860b97d9b
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-09-14 19:00:48 +0200

    another try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,8 +61,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "the_toad_production"
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: 'boy-and-toad.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "boy-and-toad.herokuapp.com", :protocol => 'https' }
   config.action_mailer.raise_delivery_errors = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
