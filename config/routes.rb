afa0835817d68ae4b7f9c06d0815759683316ab4
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-09-14 17:55:24 +0200

    Mailer functional

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'confirmations' }
   root to:'pages#home';
 
   get '/bio', to: 'pages#bio';
