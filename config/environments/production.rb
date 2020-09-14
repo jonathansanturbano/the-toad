3f046e58e4c2372e70240685248006049e1ee144
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-09-14 18:08:48 +0200

    production changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "boy-and-toad.herokuapp.com" }
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
